327904fd3a75b52d4b52293090361f401c943541
Usman Bashir <drusmanbashir@gmail.com>
2024-06-24 22:35:45 +0100

    upda

--- R/to_percent.r
+++ R/to_percent.r
@@ -1,6 +1,6 @@
 #' Convert float to percentage (text)'
 #'
-#'This is a basic function
+#' This is a basic function
 #'
 #' @param x input float (e.g., .78)
 #' @param digits number of digits (4 is usual
@@ -10,3 +10,14 @@ to_percent <- function(x, digits = 4) {
   return(paste0(format(x, digits = digits), "%"))
 }
 
+#' Convert a 3-vector to range with parenthesis, i.e., 1(2-3) '
+#'
+#' This is a basic function
+#'
+#' @param r input vector of length 3
+#' @export
+
+parenthesize <- function(r) {
+  g <- paste0(round(r[1], 3), "(", round(r[2], 3), "-", round(r[3], 3), ")")
+  return(g)
+}
